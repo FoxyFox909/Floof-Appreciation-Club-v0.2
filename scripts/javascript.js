8d9fbffe97ad77b918534092a74b9c24ebad733f
FoxyFox909 <110856509+FoxyFox909@users.noreply.github.com>
2023-02-25 02:12:10 -0500

    Add files via upload

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -38,6 +38,7 @@ function printHeaders() {
 	}
 //console.log(col);
 const table = document.createElement('table');
+table.setAttribute('id', 'GeneratedTable');
 
 const tr = table.insertRow(-1);
 
@@ -50,19 +51,46 @@ for (let i = 0; i < col.length; i++) {
 	}
 	
 
-
-
 //add the actual data from the JSON file as rows
-let rowLength = ++length;
+var rowLength = ++length;
 for (let a = 0; a < rowLength; a++) {
 		let tr = table.insertRow(-1);
 		
-		for (let j = 0; j < (col.length - 0); j++) {
+		for (let j = 0; j < (col.length - 1); j++) {
 			let tabCell = tr.insertCell(-1)
 			tabCell.innerHTML = foxArray[a][col[j]];
 		}
 	}
+	
+	
+//thumbnail column handler	
+setTimeout(() => {
+	for (let b = 0; b < rowLength; b++) {
+		let genTable = document.getElementById('GeneratedTable');
+		let newImg = document.createElement('img');
+		
+		console.log(rowLength);
+		newImg.src=foxArray[b]['thumbnail image'];
+		newImg.setAttribute('id', ('Fox' + b));
+		console.log(foxArray[b]['thumbnail image']);
+		
+		//empty cell and id generation
+		let tabCell = genTable.rows[(1+b)].insertCell(-1);
+		tabCell.setAttribute('id', ('Fox' + b));
+		
+		
+		//append children images to each new cell element
+		document.getElementById('Fox' + b).appendChild(newImg);
 
+				
+	}
+	
+	
+	//document.getElementById('GeneratedTable').rows[1].insertCell(2);
+	}, '10')
+//console.log(foxArray[1]['thumbnail image']);
+
+//--------------------------------------------------------
 
 
 
@@ -75,208 +103,25 @@ divShowFoxes.appendChild(table);
 
 
 
-//data/json-test.json
-
-/*function call(input) {
-	return new Promise((resolve, reject) => {
-		return resolve({
-			val: input
-		})
-	})
-}
-
-async function test() {
-	const res = await call("data/json-test.json")
-	console.log(res.val)
-console.log(res["val"])
-}
-
-function doJSON() {
-	console.log("test running");
-	test();
-}
-*/
-
-
-/*function load (url) {
-	return new Promise(async function (resolve, reject) {
-		const res = await fetch(url);
-		
-		console.log('Loaded: ', url);
-		
-		
-		resolve(res.json());
-		})
-	}
-
-//const promise = load('data/json-test.json');
-const promise = load('data/json-test.json');
-
-function doJSON() {
-	console.log('doJSON function loaded')
-	let savedData = promise;
-	console.log(savedData);
-	console.log(typeof savedData);
-	//refinedJSON = promise.resolve().then((result) => {console.log(result)});
-	let refinedJSON = promise.valueOf;
-	console.log(refinedJSON);
-}
-*/
 
 
 
 
 
-//https://www.encodedna.com/javascript/populate-json-data-to-html-table-using-javascript.htm
-//https://www.freecodecamp.org/news/how-to-read-json-file-in-javascript/
+
 
 /*
-function load (url) {
-	return new Promise(async function (resolve, reject) {
-		const res = await fetch(url);
-		
-		console.log('Loaded: ', url);
-		
-		
-		resolve(res.json());
-		})
+IMAGE HANDLER PROTOTYPE
+setTimeout(() => {
+	for (let b = 0; b < rowLength; b++) {
+		let genTable = document.getElementById('GeneratedTable');
+		console.log(rowLength);
+		let tabCell = genTable.rows[(1+b)].insertCell(-1);
+		tabCell.innerHTML = foxArray[b]['thumbnail image'];
+		//document.createElement('table').insertRow(-1).insertCell(-1).innerHTML = foxArray[b]['thumbnail image'];		
 	}
-
-//const promise = load('data/json-test.json');
-const promise = load('data/json-test.json');
-
-function doJSON() {
-	console.log('doJSON function loaded')
-	let savedData = promise;
-	console.log(savedData);
-	console.log(typeof savedData);
-	refinedJSON = promise.resolve().then((result) => {console.log(result)});
-	console.log(refinedJSON);
-}
-*/
-
-
-
-
-
-
-
-
-//var storedPromise = fetchJSON();
-
-/* 
-function fetchJSON() {
-		fetch('data/json-test.json')
-		.then(response => response.json()).
-		then((fox) =>{
-			//console.log(fox);
-			return fox;
-		});
-}
-
-
-const printJSON = async () => {
-	const a = await fox;
-	console.log(a);
-};
-
-*/
-
-/*
-
-function printJSON() {
-	var storedPromise = fetchJSON();
-	console.log('No Delay');
-	storedPromise.then(({common name}) => console.log({common name});
-}
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*var savedData = "";
-
-
-
-async function fetchJSON() {
-	fetch('data/json-test.json')
-		.then((response) => response.json())
-		.then((data) => console.log(data));
-	
-}
-
-async function saveJSON() {
-	let fetchedData = await fetchJSON();
-	//setTimeout(() => {console.log(fetchedData)}, 500);
-	savedData += fetchedData;
-}
-
-function processJSON() {
-	fetchJSON();
-	saveJSON();
-}
-
-function printJSON() {
-	console.log(savedData);
-}
-
-*/
-
-
-
 	
 	
-
-/* 
-
-
-	function printJSON() {
-	fetch('data/json-test.json')
-		.then((response) => response.json())
-		.then((data) => console.log(data));
-	
-}
-
-
-
-
-
-	async function printJSON() {
-	fetch('data/json-test.json')
-		.then(function (response) {
-		response.json();
-		return response;
-		})
-		.then(function (data) {
-			console.log(data);
-		};
-		
-	
-}
-
-async function writeinVar(){
-	let array = await printJSON();
-	console.log(array);
-}
-
-async function fetchData(){
-	let response = await fetch('data/json-test.json');
-	let data = await response.json();
-	data = JSON.stringify(data);
-	data = JSON.parse(data);
-	return data;
-}
-
-let testdata = await fetchData();
-console.log(testdata);
+	//document.getElementById('GeneratedTable').rows[1].insertCell(2);
+	}, '10')
 */
